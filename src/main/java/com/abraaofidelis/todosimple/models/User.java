6a88c831d51de9620a763e9f370d1d6a9131b6dd
abraaofidelis <abraaofidelis@hotmail.com>
2023-11-27 14:39:14 -0300

    Task Model creation

--- src/main/java/com/abraaofidelis/todosimple/models/User.java
+++ src/main/java/com/abraaofidelis/todosimple/models/User.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.*;
 
@@ -43,10 +44,12 @@ public class User {
 	@Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
 	private String password;
 	
+	@OneToMany(mappedBy = "user")
+	private List<Task> tasks = new ArrayList<Task>();
 	
-	// private List<Task> tasks = new ArrayList<Task>();
-	
-	public User() {} //Construtor vazio
+	public User() {
+		
+	} //Construtor vazio
 	
 	public User(Long id, String username, String password ) {
 		this.id = id;
@@ -77,6 +80,15 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Task> getTasks(){
+		return this.tasks;
+	}
+	
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
 	
 	@Override
 	public boolean equals(Object obj) {
